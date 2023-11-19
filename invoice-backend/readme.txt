9082b5e319b10e2c860ad1ece10053d80d517f07
iholtea <iholtea@gmail.com>
2023-11-19 04:17:58 +0200

    added configuration
    added data source configuration
    added spring security configuration to allow h2 console access

---
+++ invoice-backend/readme.txt
@@ -0,0 +1,15 @@
+https://app.diagrams.net/ used to be draw.io
+	online tool to create diagrams
+
+Flyway  
+	helps maintain the database, keeps a database changes history that allows rollbacks, etc
+	Provide the SQL script - create tables, add data in 
+	V1_0__init.sql file inside the src/main/resources/db/migration directory
+	Flyway naming convention is V<version>__<name>.sql ex V1_0__init.sql
+	
+Access the H2 database: http://localhost:8080/h2-console/
+
+
+	
+	
+				 
---
+++ invoice-backend/src/main/java/io/ionuth/invoice/configuration/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package io.ionuth.invoice.configuration;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+		/*
+		 *  PathRequest.toH2Console() is equivalent to 
+		 *  new AntPathRequestMatcher("/h2-console/**") is 
+		 */
+		http
+			.csrf( csrf -> csrf
+					.ignoringRequestMatchers(PathRequest.toH2Console())
+					.disable()
+			)
+			.authorizeHttpRequests( auth -> auth
+				.requestMatchers(PathRequest.toH2Console()).permitAll()
+				
+			)
+			.headers( headers -> headers.frameOptions(FrameOptionsConfig::disable) );
+		
+		return http.build();
+	}
+	
+}
