7afdc3d8050e4007849800eeff837d1eff81c16f
iholtea <iholtea@gmail.com>
2023-11-19 02:01:50 +0200

    initial commit of spring boot backend

---
+++ invoice-backend/src/test/java/io/ionuth/invoice/InvoiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ionuth.invoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
